4cfb8c436c2f3dc9cd09e5991db9f3f2fd88f718
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2025-10-31 09:25:58 +0530

    Completed the routing and added roles

---
+++ src/middlewares/auth-request-validator.js
@@ -0,0 +1,28 @@
+const validateUserAuth = (res, req, next) => {
+    if(!req.body.email || !req.body.password) {
+        return res.status(400).json({
+            success: false,
+            data: {},
+            message: 'Something went wrong',
+            err: 'Email or Password missing in the request'
+        })
+    }
+    next();
+}
+
+const validateIsAdminRequest = (req, res, next) => {
+    if(!req.body.id) {
+        return res.status(400).json({
+            success: false,
+            data: {},
+            err: 'User id not given',
+            message: 'Something went wrong'
+        })
+    }
+    next();
+}
+
+module.exports = {
+    validateUserAuth,
+    validateIsAdminRequest
+}
--- src/models/user.js
+++ src/models/user.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.Roles, {
+        through: 'User_Roles',
+      }) 
     }
   }
   User.init({
