b050264cc5d28663b09e72d788cd802a8a533f52
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2025-10-31 07:46:20 +0530

    Completed setting up user for the application

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+
+const db = require('./models/index');
+
+const app = express();
+
+const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, async () => {
+        console.log(`Server started on PORT ${PORT}`);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+    })
+}
+
+prepareAndStartServer();
